52f7a613bba66162366c1bcb49c0177cbc91740a
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-08-10 11:29:51 +0000

    Generate graphiti resources for directors

---
+++ app/resources/director_resource.rb
@@ -0,0 +1,12 @@
+class DirectorResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :dob, :datetime
+
+  # Direct associations
+
+  # Indirect associations
+
+end
